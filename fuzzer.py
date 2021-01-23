ea1f3a2d97a9f76e709ccca8e1717ca0d8b68964
kyomukyomupurin <murakami1728@gmail.com>
2021-01-23 19:40:47 +0900

    :recycle: Refactor code

--- fuzzer.py
+++ fuzzer.py
@@ -1,35 +1,37 @@
 import subprocess
 import sys
+from pathlib import Path
 
 
 def fuzz():
-    subprocess.run("./gen", stdout=open("./gen.txt", "w"))
-    sol_out = subprocess.run("./sol", check=True, stdin=open("./gen.txt", "r"), stdout=subprocess.PIPE).stdout
-    stupid_out = subprocess.run("./stupid", check=True, stdin=open("./gen.txt", "r"), stdout=subprocess.PIPE).stdout
+    subprocess.run("./gen", stdout=Path("gen.txt").open("w"))
+    sol_out = subprocess.run("./sol", check=True, stdin=Path("gen.txt").open("r"), stdout=subprocess.PIPE).stdout
+    stupid_out = subprocess.run("./stupid", check=True, stdin=Path("gen.txt").open("r"), stdout=subprocess.PIPE).stdout
 
     if sol_out != stupid_out:
         print("[test {:<3}] : NG".format(i + 1))
 
-        open("./sol.txt", "w").write(sol_out.decode("utf-8"))
-        open("./stupid.txt", "w").write(stupid_out.decode("utf-8"))
+        Path("sol.txt").open("w").write(sol_out.decode("utf-8"))
+        Path("stupid.txt").open("w").write(stupid_out.decode("utf-8"))
 
         print("Input : ")
-        subprocess.run("cat ./gen.txt", shell=True)
+        subprocess.run(["cat", "gen.txt"])
         print("Expected : ")
-        subprocess.run("cat ./stupid.txt", shell=True)
+        subprocess.run(["cat", "stupid.txt"])
         print("Found : ")
-        subprocess.run("cat ./sol.txt", shell=True)
+        subprocess.run(["cat", "sol.txt"])
 
         exit(0)
     else:
         print("[test {:<3}] : OK".format(i + 1))
 
-if __name__ == '__main__':
-    subprocess.run(["make sol"], shell=True)
-    subprocess.run(["make stupid"], shell=True)
-    subprocess.run(["make gen"], shell=True)
+
+if __name__ == "__main__":
+    subprocess.run(["make", "sol"])
+    subprocess.run(["make", "stupid"])
+    subprocess.run(["make", "gen"])
 
     for i in range(100 if len(sys.argv) < 2 else int(sys.argv[1])):
         fuzz()
 
-    subprocess.run(["make clean"], shell=True)
+    subprocess.run(["make", "clean"])
